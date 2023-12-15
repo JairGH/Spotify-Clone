e0fdcda56acc86e9eb489ccb08c8004c5c52f40c
Jair Gomez <jairherber505@gmail.com>
2023-12-14 17:40:26 -0800

    play btn works by id now

--- src/components/CardPlayBtn.jsx
+++ src/components/CardPlayBtn.jsx
@@ -2,19 +2,25 @@ import { Play, Pause } from "./Player";
 import { usePlayerStore } from "@/store/playerStore";
 
 export function CardPlayBtn({ id }) {
-  const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(
-    (state) => state
-  );
+  const { currentMusic, isPlaying, setIsPlaying, setCurrentMusic, volume } =
+    usePlayerStore((state) => state);
   const handleClick = () => {
+    setCurrentMusic({
+      playlist: {
+        id,
+      },
+    });
     setIsPlaying(!isPlaying);
   };
+
+  const isPlayingPlaylist = isPlaying && currentMusic?.playlist.id === id;
 
   return (
     <button
       onClick={handleClick}
       className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400"
     >
-      {isPlaying ? <Pause /> : <Play />}
+      {isPlayingPlaylist ? <Pause /> : <Play />}
     </button>
   );
 }
